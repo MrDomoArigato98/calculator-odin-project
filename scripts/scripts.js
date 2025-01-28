96cd4a35b2e4a5d1051547fc1ac26ea9014015db
MrDomoArigato98 <domdob98@gmail.com>
2025-01-28 10:09:29 +0000

    Creates operator function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,7 @@
+
+let num1;
+let num2;
+let operator;
 //add = () =>{}
 function add(num1, num2){
     return num1+num2
@@ -10,5 +14,22 @@ function subtract(num1,num2){
 
 //multiply = () => {}
 function multiply(num1,num2){
+    return num1*num2
+}
 
+//divide = () => {}
+function divide(num1, num2){
+    return num1 / num2
+}
+
+function operate(num1,operator,num2){
+    if(operator=='+'){
+        return add(num1,num2)
+    }else if (operator=='-'){
+        return subtract(num1,num2)
+    }else if (operator == '*'){
+        return multiply(num1,num2)
+    }else if (operator == '/'){
+        return divide(num1,num2)
+    }
 }
