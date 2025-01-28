0f2e96ef7f1afa8d31942ea1476bdcf7d0fe3345
MrDomoArigato98 <domdob98@gmail.com>
2025-01-28 10:03:45 +0000

    Adds, subtracts, multiplies - function

---
+++ scripts/scripts.js
@@ -0,0 +1,14 @@
+//add = () =>{}
+function add(num1, num2){
+    return num1+num2
+}
+
+//subtract = () => {}
+function subtract(num1,num2){
+    return num1-num2
+}
+
+//multiply = () => {}
+function multiply(num1,num2){
+
+}
