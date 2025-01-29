012f7cd594dc59955aebc981f445f070a3873f6f
MrDomoArigato98 <domdob98@gmail.com>
2025-01-29 10:43:12 +0000

    Working on happy path first
    Difficult exercise to get finished, I'll work on happy path of execution and fix any bugs afterwards

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -29,6 +29,18 @@ function divide(num1, num2){
 }
 
 function operate(){
+    if(firstInput && secondInput &! secondOperator){
+     console.log('1st input, second input NO 2nd operator')
+     switch (firstOperator) {
+        case 'plus':
+            result = add(Number(firstInput), Number(secondInput))
+            displayValue = result
+            break;
+     
+        default:
+            break;
+     }
+    }
 }
 
 
@@ -45,32 +57,67 @@ function clearDisplay(){
     display.textContent=''
 }
 
+
+console.table(buttonsArray)
+console.table(display)
+
 function setDisplay(value){
-    display.textContent+=value
+    display.textContent = displayValue;
 }
+setDisplay(display)
 
 function setOperator(operator){
-    if(firstOperator==null && secondOperator==null){
-        firstOperator=operator
 
-    }else if(firstOperator!=null && secondOperator==null){
-        console.log("got second operator")
-        secondOperator=operator
-    }
+    firstOperator = operator
+    // if(firstOperator==null && secondOperator==null){
+    //     firstOperator=operator
+
+    // }else if(firstOperator!=null && secondOperator==null){
+    //     console.log("got second operator")
+    //     secondOperator=operator
+    // }
+
 }
 function setInput(displayContent){
-
+    if(firstInput==null){
+        firstInput=displayContent
+        console.log("Set first input: "+firstInput)
+    }else if(secondInput==null){
+        console.log("Set second input: "+secondInput)
+        secondInput=displayContent
+    }
 }
 
 buttonsArray.forEach(function(elem){
     elem.addEventListener("click", function(){
-        if(elem.classList.contains('operand')){
-            console.log(elem.value)
+        if(elem.classList.contains('number')){
+            console.log("Clicked number: " + elem.value)
+            setDisplay(elem.value)
         }
 
-        if(elem.classList.contains('action')){
-            console.log(elem.id)
+        if(elem.id == "equals"){
+            setInput(display.textContent)
+            operate()
         }
-       
+
+        if(elem.id == "plus"){
+            console.log(display.textContent)
+            setInput(display.textContent)
+            setOperator("plus")
+            clearDisplay()
+        }
+
+        if(elem.id == "minus"){
+
+        }
+
+        if(elem.id == "multiply"){
+        }
+        if(elem.id == "divide"){
+        }
+        if(elem.id == "clear"){
+            allClear()
+            display.textContent=0
+        }
     })
 })
